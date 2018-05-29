51f229f19f99d2eea206b55dee8242b5e8f08dd4
LuisCavalari <lfelipe.felipe4@gmail.com>
2018-05-28 21:43:06 -0300

    First commit

---
+++ login.py
@@ -0,0 +1,8 @@
+usr = raw_input("Digite o nome de usuario\n>>>")
+psw = raw_input("Digite a senha\n>>>")
+if usr == "root" and psw == "toor":
+        print "Logged"
+elif usr == "feanor" and psw =="assembly":
+        print "Logged"
+else:
+    print"Usuario ou senha incorretos"
