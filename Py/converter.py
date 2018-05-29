2cc3ea97cf686bf167dcc6e53cb8a2557fb66ef7
LuisCavalari <lfelipe.felipe4@gmail.com>
2018-05-29 10:32:52 -0300

    new file, converter, transform celius to fahrenheint, and reverse too

---
+++ Py/converter.py
@@ -0,0 +1,16 @@
+
+op=0
+while(op!=3):
+    op = int(raw_input("Escolha uma opcao \n 1-Celsius para fahreinheint \n 2-fahreinhein para Celius \n 3-sair \n>>>  " ))
+    if(op==1):
+        fh = float(raw_input("Digite o valor em fahreinheint  \n>>>"))
+        c = ((fh-32)/9)*5
+        print("O valor em celsius e  %f"%(c))
+    elif(op==2):
+        cl = float(raw_input("Digite o valor em celsius \n>>> "))
+        rfh = ((cl/5)*9)+32
+        print("O valor em celsius e  %f"%(rfh))
+    elif op==3:
+        print "Saindo...."
+    else:
+        print"Op invalida"
